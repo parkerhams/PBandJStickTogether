590ed1e6f97d9a95fae874a137920e2daf4842ce
Parker Hamilton <PARKER.HAMILTON@iam.local>
2018-04-16 13:57:53 -0500

    Bouncable level, pickup lapha - but cannot pick up for some reason? 3D Text to do text bob but does not work yet

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickups : MonoBehaviour 
+{
+    [SerializeField]
+    GameObject pickupText;
+
+    private void Start()
+    {
+        pickupText.SetActive(false);
+    }
+
+    public void OnCollisionEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("PeanutButter"))
+        {
+            Destroy(this.gameObject);
+            pickupText.SetActive(true);
+        }
+    }
+}
