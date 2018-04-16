99e09eefad0cdd3baaaf0c83f23d8f1b1767c806
parkerhams <parker.hamilton@loop.colum.edu>
2018-04-16 01:31:23 -0500

    Added kitchen assets, created UI screen

---
+++ Assets/Scripts/TextBob.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBob : MonoBehaviour {
+
+    public Transform start;
+    public Transform end;
+
+    public float factor = 1.0f;
+
+    void Start()
+    {
+        SetPos(start.position, end.position);
+    }
+
+    void Update()
+    {
+        SetPos(start.position, end.position);
+    }
+
+    void SetPos(Vector3 start, Vector3 end)
+    {
+        var dir = end - start;
+        var mid = (dir) / 2.0f + start;
+        transform.position = mid;
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        Vector3 scale = transform.localScale;
+        scale.y = dir.magnitude * factor;
+        transform.localScale = scale;
+    }
+}
