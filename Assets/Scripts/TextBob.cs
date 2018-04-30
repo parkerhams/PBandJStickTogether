13b2b34aa92be54e36a85b619a2a4cb35d09a3ff
parkerhams <parker.hamilton@loop.colum.edu>
2018-04-30 06:57:15 -0500

    TEXT BOB WORKS YAYAYAYAYAYAY

--- Assets/Scripts/TextBob.cs
+++ Assets/Scripts/TextBob.cs
@@ -5,29 +5,38 @@ using UnityEngine;
 public class TextBob : MonoBehaviour
 {
 
-    public Transform start;
-    public Transform end;
-
-    public float factor = 1.0f;
-
-    void Start()
-    {
-        SetPos(start.position, end.position);
-    }
+    public float speed = 2f;
+    public float maxRotation = 45f;
 
     void Update()
     {
-        SetPos(start.position, end.position);
+        transform.rotation = Quaternion.Euler(maxRotation * Mathf.Sin(Time.time * speed), 0f, 0f);
     }
 
-    void SetPos(Vector3 start, Vector3 end)
-    {
-        var dir = end - start;
-        var mid = (dir) / 2.0f + start;
-        transform.position = mid;
-        transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
-        Vector3 scale = transform.localScale;
-        scale.y = dir.magnitude * factor;
-        transform.localScale = scale;
-    }
+
+    //public Transform start;
+    //public Transform end;
+
+    //public float factor = 1.0f;
+
+    //void Start()
+    //{
+    //    SetPos(start.position, end.position);
+    //}
+
+    //void Update()
+    //{
+    //    SetPos(start.position, end.position);
+    //}
+
+    //void SetPos(Vector3 start, Vector3 end)
+    //{
+    //    var dir = end - start;
+    //    var mid = (dir) / 2.0f + start;
+    //    transform.position = mid;
+    //    transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+    //    Vector3 scale = transform.localScale;
+    //    scale.y = dir.magnitude * factor;
+    //    transform.localScale = scale;
+    //}
 }
